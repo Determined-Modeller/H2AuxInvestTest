f2ee11d702c64a50b0912059966dc378e91b1da6
EC2 Default User <ec2-user@ip-172-31-14-182.eu-north-1.compute.internal>
2024-01-08 18:21:38 +0000

    UIv6

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import nodePolyfills from "rollup-plugin-polyfill-node";
 export default defineConfig({
   plugins: [react()],
+
+  define: {
+    global: {},
+  },
   resolve: {
-      alias: [
-      {
-        find: './runtimeConfig',
-        replacement: './runtimeConfig.browser', // ensures browser compatible version of AWS JS SDK is used
-      },
-    ]
-  }
-})
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser",
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [nodePolyfills()],
+    },
+  },
+});
