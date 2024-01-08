39145ed9b89b3e3c030daedb3b7b0f5b128a1785
EC2 Default User <ec2-user@ip-172-31-14-182.eu-north-1.compute.internal>
2024-01-08 13:11:03 +0000

    UITestv3

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, Flex, Heading, Image, Text } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Flex
+      direction={{ base: 'column', large: 'row' }}
+      maxWidth="32rem"
+      padding="1rem"
+      width="100%"
+    >
+      <Image
+        alt="Abstract art"
+        height="21rem"
+        objectFit="cover"
+        src="https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987"
+      />
+      <Flex justifyContent="space-between" direction="column">
+        <Heading level={3}>Abstract art</Heading>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat
+          sed cras ornare arcu dui. Duis aute irure dolor in reprehenderit in
+          voluptate velit esse.
+        </Text>
+        <Button
+          variation="primary"
+          onClick={() => alert('Added item to cart!')}
+        >
+          Add to Cart
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
 
-export default App
+export default App;
