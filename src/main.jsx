c538aed92a55b2f7f6968377db055b09b01c6339
EC2 Default User <ec2-user@ip-172-31-14-182.eu-north-1.compute.internal>
2024-01-08 15:24:02 +0000

    UIv2

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,10 @@ import { ThemeProvider } from "@aws-amplify/ui-react";
 import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports';
 import "@aws-amplify/ui-react/styles.css";
-import { studioTheme } from "./ui-components";
+import studioTheme from "./ui-components";
 
 Amplify.configure(awsconfig);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+<ThemeProvider theme={studioTheme}>
     <App />
-  </React.StrictMode>,
-)
+</ThemeProvider>
