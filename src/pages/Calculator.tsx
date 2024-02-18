95a815eb6604f9b2aaa9239bce60c294c1202fe6
Adam Bystran <bys.adam@gmail.com>
2024-02-18 20:25:28 +0000

    #7 creates initial calculator landing page

--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -1,24 +1,29 @@
 
 
-import { Box, Input } from "@mui/joy";
+import { Box, Button, Typography, useColorScheme } from "@mui/joy";
 
-import { useForm, SubmitHandler } from "react-hook-form"
+// import { useForm, SubmitHandler } from "react-hook-form"
+import Diagram from '../assets/diagram_input.png';
+import DiagramInverse from '../assets/diagram_inverse.png';
+import { ArrowForward } from "@mui/icons-material";
+import ROUTE_CONSTANTS from "../routing/routeConstants";
 
-type Inputs = {
-    example: string
-    exampleRequired: string
-}
+// type Inputs = {
+//     example: string
+//     exampleRequired: string
+// }
 
 const Calculator = () => {
-    const {
-        register,
-        handleSubmit,
-        watch,
-        formState: { errors },
-    } = useForm<Inputs>()
-    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+    const { mode } = useColorScheme();
+    // const {
+    //     register,
+    //     handleSubmit,
+    //     watch,
+    //     formState: { errors },
+    // } = useForm<Inputs>()
+    // const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
 
-    console.log(watch("example")) // watch input value by passing the name of it
+    // console.log(watch("example")) // watch input value by passing the name of it
 
     return (
         <Box
@@ -26,17 +31,94 @@ const Calculator = () => {
                 minHeight: '100vh',
             }}
         >
-            <form onSubmit={handleSubmit(onSubmit)}>
-                {/* register your input into the hook by invoking the "register" function */}
+            <Box
+                py={'50px'}
+                sx={{
+                    maxWidth: '800px',
+                    margin: 'auto'
+                }}
+            >
+                <Typography level="h3" color="primary" fontSize="lg" fontWeight="lg">
+                    H2 Aux Invest Calculator
+                </Typography>
+                <Typography level="h2">
+                    Let's calculate [need some text here]
+                </Typography>
+            </Box>
+            <Box
+                sx={(theme) => ({
+                    marginTop: '50px',
+                    borderWidth: 3,
+                    borderRadius: 'sm',
+                    bgcolor: 'background.surface',
+                    maxWidth: '800px',
+                    margin: 'auto',
+                    boxShadow: theme.shadow.xl,
+                    '--joy-shadowChannel': theme.vars.palette.primary.mainChannel,
+                })}
+            >
+                <Box
+                    py={'50px'}
+                    sx={{
+                        maxWidth: '400px',
+                        margin: 'auto'
+                    }}
+                >
+                    {mode === 'light' ? <img src={Diagram} width={'100%'} /> : <img src={DiagramInverse} width={'100%'} />}
+                </Box>
+            </Box>
+
+            <Box
+                py={'50px'}
+                sx={{
+                    maxWidth: '800px',
+                    margin: 'auto'
+                }}
+            >
+                <Typography>
+                    [short description of the inputs/process and what the calculator does]
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Diam vel quam elementum
+                    pulvinar etiam. Eu augue ut lectus arcu bibendum at varius vel. Tortor vitae
+                    purus faucibus ornare suspendisse sed nisi. A pellentesque sit amet porttitor.
+                </Typography>
+                <Box
+                    sx={{
+                        maxWidth: "400px",
+                        display: 'flex',
+                        flexWrap: 'wrap',
+                        gap: 2,
+                        my: 7,
+                        '& > *': { flex: 'auto' },
+                    }}
+                >
+                    <Button
+                        component="a"
+                        href={ROUTE_CONSTANTS.DOCS}
+                        size="lg" variant="outlined" color="neutral">
+                        Learn More
+                    </Button>
+                    <Button
+                        component="a"
+                        href={ROUTE_CONSTANTS.CALCULATOR}
+                        size="lg"
+                        endDecorator={<ArrowForward />}>
+                        Start
+                    </Button>
+                </Box>
+            </Box>
+
+            {/* <form onSubmit={handleSubmit(onSubmit)}>
+
                 <Input defaultValue="test" {...register("example")} />
 
-                {/* include validation with required or other standard HTML validation rules */}
+
                 <Input {...register("exampleRequired", { required: true })} />
-                {/* errors will return when field validation fails  */}
+
                 {errors.exampleRequired && <span>This field is required</span>}
 
                 <Input type="submit" />
-            </form>
+            </form> */}
         </Box>
     );
 }
