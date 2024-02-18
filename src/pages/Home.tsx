793076de4380171ef473a2fab12c90c2f13914ce
Adam Bystran <bys.adam@gmail.com>
2024-02-18 17:49:35 +0000

    #6 Adds links from main page to calculator and home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
 
 
-import { Box } from "@mui/joy"
-import HeroLeft01 from "../blocks/HeroLeft01"
+import { Box, Button, Typography } from "@mui/joy"
+import { ArrowForward } from "@mui/icons-material";
+import TwoSidedLayout from "../components/TwoSidedLayout";
+import ROUTE_CONSTANTS from "../routing/routeConstants";
 
 
 
@@ -12,7 +14,44 @@ const Home = () => {
                 minHeight: '100vh',
             }}
         >
-            <HeroLeft01 />
+            <TwoSidedLayout>
+                <Typography color="primary" fontSize="lg" fontWeight="lg">
+                    Hydrogen Planning Tool
+                </Typography>
+                <Typography
+                    level="h1"
+                    fontWeight="xl"
+                    fontSize="clamp(1.875rem, 1.3636rem + 2.1818vw, 3rem)"
+                >
+                    Estimate capital cost of operating a hydrogen dispensing station.
+                </Typography>
+                <Typography fontSize="lg" textColor="text.secondary" lineHeight="lg">
+                    Get a quick estimate of operating, capital, and installation cost of your H2 dispensing station plans.
+                </Typography>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexWrap: 'wrap',
+                        gap: 2,
+                        my: 2,
+                        '& > *': { flex: 'auto' },
+                    }}
+                >
+                    <Button
+                        component="a"
+                        href={ROUTE_CONSTANTS.DOCS}
+                        size="lg" variant="outlined" color="neutral">
+                        Learn More
+                    </Button>
+                    <Button
+                        component="a"
+                        href={ROUTE_CONSTANTS.CALCULATOR}
+                        size="lg"
+                        endDecorator={<ArrowForward />}>
+                        Get Started For Free
+                    </Button>
+                </Box>
+            </TwoSidedLayout>
         </Box>
     );
 }
