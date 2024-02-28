22adccd279c912d7b4f3680def60f94dafea1c5b
Adam Bystran <bys.adam@gmail.com>
2024-02-28 21:38:57 +0000

    Adds more detail to the api spec and configures a mock reposne

--- amplify/backend/function/h2auxcalculator/src/index.py
+++ amplify/backend/function/h2auxcalculator/src/index.py
@@ -7,24 +7,22 @@ from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 from aws_lambda_powertools.logging import correlation_paths
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
+from service.calculator_service import CalculatorService
+
 tracer = Tracer()
 logger = Logger()
-app = APIGatewayRestResolver(enable_validation=True)  
-
-
-class Test(BaseModel):  
-    userId: int
-    id_: Optional[int] = Field(alias="id", default=None)
+app = APIGatewayRestResolver(enable_validation=False)  
+calculatorService = CalculatorService()
 
 
 @app.post("/calculator/calculate")
-def calculate(todo: Test) -> str:  
-    return json.dumps("GET TODOS")
+def calculate() -> str:  
+    return json.dumps(calculatorService.calculate("test"))
 
 
 @app.get("/calculator")
 def home() -> str:  
-    return json.dumps("H2AUX API")
+    return json.dumps(calculatorService.calculate({}))
 
 
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_HTTP)
