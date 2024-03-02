52b04e9dd9291ec765732ee896db7b2bff56ea1f
Adam Bystran <bys.adam@gmail.com>
2024-03-02 21:28:55 +0000

    testing pre push

--- amplify/hooks/pre-push.js
+++ amplify/hooks/pre-push.js
@@ -1,21 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 const fs = require('fs');
-const parameters = JSON.parse(fs.readFileSync(0, { encoding: 'utf8' }));
-const oas = fs.readFileSync(__dirname + '\\..\\backend\\api\\h2AuxCalculatorApi\\openapi.json')
-console.log(oas);
-// Get the running Amplify CLI major version number
-const currentCLIMajorVersion = parameters.data.amplify.version.split('.')[0]
-console.log('Amplify CLI major version: ', currentCLIMajorVersion)
+const testFolder = __dirname + '\\..\\';
 
-const MINIMUM_MAJOR_AMPLIFY_CLI_VERSION = 5
-console.log('Minimum required Amplify CLI major version: ', MINIMUM_MAJOR_AMPLIFY_CLI_VERSION)
-
-if (currentCLIMajorVersion < MINIMUM_MAJOR_AMPLIFY_CLI_VERSION) {
-    // Non-zero exit code will stop the Amplify CLI command's execution
-    console.log('Minimum CLI version requirement not met.')
-    process.exit(1)
-} else {
-    console.log('Minimum CLI version requirement met.')
-    process.exit(0)
-}
+fs.readdir(testFolder, (err, files) => {
+    files.forEach(file => {
+        console.log(file);
+    });
+});
