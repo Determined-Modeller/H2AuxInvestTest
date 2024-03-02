e97baa6f004f8997c01f853b4fae8ed7648e07bc
Adam Bystran <bys.adam@gmail.com>
2024-03-02 21:42:28 +0000

    testing pipeline

--- amplify/hooks/pre-push.js
+++ amplify/hooks/pre-push.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const path = require("path")
 
-const testFolder = path.dirname(path.basename(__dirname));
+const testFolder = path.dirname(path.basename(__dirname)) + "/amplify/backend/api/h2AuxCalculatorApi";
 
 fs.readdir(testFolder, (err, files) => {
     files.forEach(file => {
