07f194df01cb523cad6c4025c4eb4b1f8ab56794
Adam Bystran <bys.adam@gmail.com>
2024-03-02 21:32:10 +0000

    test pipeline

--- amplify/hooks/pre-push.js
+++ amplify/hooks/pre-push.js
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 const fs = require('fs');
-const testFolder = __dirname + '\\..\\';
+const testFolder = __dirname;
 
 fs.readdir(testFolder, (err, files) => {
     files.forEach(file => {
