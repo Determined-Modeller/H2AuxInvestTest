81515341e3bd65f96f598f2d2628f1d75e0a1756
culmajster <129602452+culmajster@users.noreply.github.com>
2024-03-05 21:43:33 +0000

    Update object_meta

--- amplify/backend/function/h2auxcalculator/src/mapper/objects_meta.py
+++ amplify/backend/function/h2auxcalculator/src/mapper/objects_meta.py
@@ -15,17 +15,6 @@ meta['dispenser'] = {
                 ]    
             }
 
-meta['new dispenser'] = {
-                "title": "New Dispenser",
-                "description": "This is a dispenser",
-                "positives": [
-                    "New dispenser type coming soon"
-                ],
-                "negatives": [
-                    "New dispenser type coming soon"
-                ]    
-            }
-
 meta['piston compressor'] = {
                 "title": "Piston Compressor",
                 "description": "This is a piston compressor",
@@ -65,17 +54,6 @@ meta['centrifugal compressor'] = {
                 ]    
             }
 
-meta['new compressor'] = {
-                "title": "New Compressor",
-                "description": "This is a compressor",
-                "positives": [
-                    "New compressor type coming soon"
-                ],
-                "negatives": [
-                    "New compressor type coming soon"
-                ]    
-            }
-
 meta['type I/II storage'] = {
                 "title": "Type I/II Storage",
                 "description": "This is a type I/II storage",
@@ -99,6 +77,30 @@ meta['type III/IV storage'] = {
                 "negatives": [
                     "Something negative",
                     "Something else negative"
+                ]    
+            }
+
+
+meta['new dispenser'] = {
+                "title": "New Dispenser",
+                "description": "This is a dispenser",
+                "positives": [
+                    "New dispenser type coming soon"
+                ],
+                "negatives": [
+                    "New dispenser type coming soon"
+                ]    
+            }
+
+
+meta['new compressor'] = {
+                "title": "New Compressor",
+                "description": "This is a compressor",
+                "positives": [
+                    "New compressor type coming soon"
+                ],
+                "negatives": [
+                    "New compressor type coming soon"
                 ]    
             }
 
